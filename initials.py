e0b2e7c89838d64ac07570475ccb4788cf2f0e02
SedKing <2425400774@live.gctu.edu.gh>
2026-05-31 11:33:20 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Printing initials with Python.
+# Assignment Number: 2
+#
+# Name: DENNIS EPHRAIM SEBLANU
+# STUDENT ID:  2425400774
+# Email: 2425400774@LIVE.GCTU.EDU.GH
+# Grader: SEBLANU
+#
+# On my honor, DENNIS EPHRAIM SEBLANU, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Printing my initials S.E.D. in BLOCK Format
+    print()
+    print("...SED")
+    print()
+    print(".....SSSSSSSSSS........EEEEEEEEEEEE........DDDDDDDD.........")
+    print("....SSSSSSSSSSS........EEEEEEEEEEEE........DDDDDDDDD........")
+    print("...SSS.................EEE.................DDD.....DDD......")
+    print("...SSS.................EEE.................DDD......DDD.....")
+    print("....SSSSSSSSS..........EEEEEEEEEEEE........DDD......DDD.....")
+    print(".....SSSSSSSSS.........EEEEEEEEEEEE........DDD......DDD.....")
+    print("............SSS........EEE.................DDD......DDD.....")
+    print("............SSS........EEE.................DDD.....DDD......")
+    print("...SSSSSSSSSSS....**...EEEEEEEEEEEE...**...DDDDDDDDDD.....**")
+    print("...SSSSSSSSSS.....**...EEEEEEEEEEEE...**...DDDDDDDDD......**")
+    print()
 
 
 main()
